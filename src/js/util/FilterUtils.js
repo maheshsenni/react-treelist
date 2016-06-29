c938035f8cd672ede53d3094237e98b95d68a43e
Mahesh <mahesh.h4evr@gmail.com>
2016-06-29 08:01:33 -0500

    FIlter options for number and date types

--- src/js/util/FilterUtils.js
+++ src/js/util/FilterUtils.js
@@ -29,3 +29,55 @@ export const OPTIONS_STRING = [{
   text: 'Is not null',
   value: 'nn'
 }];
+
+export const OPTIONS_NUMBER = [{
+  text: 'Is equal to',
+  value: 'eq'
+}, {
+  text: 'Is not equal to',
+  value: 'neq'
+}, {
+  text: 'Is greater than',
+  value: 'gt'
+}, {
+  text: 'Is greater than equal to',
+  value: 'gte'
+}, {
+  text: 'Is less than',
+  value: 'lt'
+}, {
+  text: 'Is less than or equal to',
+  value: 'lte'
+}, {
+  text: 'Is null',
+  value: 'n'
+}, {
+  text: 'Is not null',
+  value: 'nn'
+}];
+
+export const OPTIONS_DATE = [{
+  text: 'Is equal to',
+  value: 'eq'
+}, {
+  text: 'Is not equal to',
+  value: 'neq'
+}, {
+  text: 'Is after',
+  value: 'af'
+}, {
+  text: 'Is on or after',
+  value: 'oaf'
+}, {
+  text: 'Is before',
+  value: 'bf'
+}, {
+  text: 'Is on or before',
+  value: 'obf'
+}, {
+  text: 'Is null',
+  value: 'n'
+}, {
+  text: 'Is not null',
+  value: 'nn'
+}];
